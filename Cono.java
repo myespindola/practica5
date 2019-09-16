11e9b57ca27cfca4c3531189b9fe4fef06f964f1
Pepe2443 <42614504+Pepe2443@users.noreply.github.com>
2019-09-16 14:17:41 -0500

    segundo archivo

--- Cono.java
+++ Cono.java
@@ -1,8 +1,8 @@
 public class Cono{
-	private double radio;
+	private double radio;//Declaración de variables
 	private double pi=3.1416;
 	private double altura;
-	public Cono (double radio, double altura){
+	public Cono (double radio, double altura){//constructor
 		this.radio=radio;
 		this.altura=altura;
 	}
@@ -10,16 +10,16 @@ public class Cono{
 		this.radio=radio;
 		this.altura=altura;
 	}
-	public double areaCono(){
+	public double areaCono(){//metodo para obtener el area superficial del cono en terminos de radio de la base y altura
 		return pi*radio*(Math.sqrt(Math.pow(radio,2)+Math.pow(altura,2))+radio);
 	}
-	public double volumenCono(){
+	public double volumenCono(){//metodo para obtener el volumen del cono en terminos del radio de la base y altura
 		return (1/3)*pi*Math.pow(radio,2)*altura;
 	}
-	public void impresionArea(){
+	public void impresionArea(){//metodo para imprimir el resultado del area
 		System.out.println("El area de tu cono es: "+areaCono());
 	}
-	public void impresionVolumen(){
+	public void impresionVolumen(){//metodo para imprimir el resultado del volumen
 		System.out.println("El volumen de tu cono es: "+volumenCono());
 	}
 }
