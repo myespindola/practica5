c558d355ed95a469b666517275bd7eb735a825be
myespindola <marcos080399@hotmail.com>
2019-09-16 14:27:05 -0500

    Primera Sincronizacion

--- PrimeraContribucion.java
+++ PrimeraContribucion.java
@@ -1,5 +1,6 @@
 public class PrimeraContribucion{
 	public static void main(String [] args){
 		System.out.println("Ponce Patiño Jose Angel");
+		System.out.println("Marcos Yañez Espindola");
 	}
 }
