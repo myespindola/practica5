35a0a807a55db8642032f56fb10ea64b18a2eb2c
thepeibol <peibol.nico@gmail.com>
2019-09-16 15:05:28 -0500

    primer comiprimer comiprimer comiit

--- PrimeraContribucion.java
+++ PrimeraContribucion.java
@@ -2,5 +2,6 @@ public class PrimeraContribucion{
 	public static void main(String [] args){
 		System.out.println("Ponce Patiño Jose Angel");
 		System.out.println("Marcos Yañez Espindola");
+		System.out.println("Pablo Nicolas Guadalupe");
 	}
 }
