5a45dbe79f11d76efbc8e7248b889deeba734294
thepeibol <peibol.nico@gmail.com>
2019-09-16 20:41:44 -0500

    Prisma con coments

--- Cuerpos Geometricos/Prisma.java
+++ Cuerpos Geometricos/Prisma.java
@@ -1,10 +1,12 @@
 public class Prisma{
+	//Declaracion de variables
 	private double med_lado;
 	private double altura;
-	private int n_lados;
+	private int n_lados;//esta variable no necesita ser de tipo de double al tratarse de un numero natural
 	private double apotem;
 
 	public Prisma(double med_lado, double altura, int n_lados, double apotem){
+		//constructor
 		this.med_lado = med_lado;
 		this.altura = altura;
 		this.n_lados = n_lados;
@@ -19,10 +21,18 @@ public class Prisma{
 	}
 
 	public double areaBase(){
+		/*
+		en este metodo obtenemos el area de la cara base de nuestro ´risma
+		el metodo funiona para cualquier tipo de prisma con numero de lados de la
+		figura de la base mayor o igual a 3
+		*/
 		return (n_lados*med_lado*apotem)/2;
 	}
 
 	public double areaLado(){
+		/*
+		En este metodo calculamos el area de unos de los lados rectangualres de nuestro prisma
+		*/
 		return med_lado*altura;
 	}
 
