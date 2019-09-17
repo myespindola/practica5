33c6281342e3a99ca9255a727109a2bb3ad22c1b
yunue20 <yunuebarrera@hotmail.com>
2019-09-16 19:29:50 -0500

    Ultima sincronizacion

--- PrimeraContribucion.java
+++ PrimeraContribucion.java
@@ -3,5 +3,6 @@ public class PrimeraContribucion{
 		System.out.println("Ponce Patiño Jose Angel");
 		System.out.println("Marcos Yañez Espindola");
 		System.out.println("Pablo Nicolas Guadalupe");//De Peibol
+		System.out.println("Yunue Hernandez Barrera");
 	}
 }
