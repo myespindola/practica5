6f39f398f62d93392dfe6b5456a9bbb84c729edd
yunue20 <yunuebarrera@hotmail.com>
2019-09-16 19:59:13 -0500

    Clase Esfera 2

--- Cuerpos Geometricos/Esfera.java
+++ Cuerpos Geometricos/Esfera.java
@@ -1,42 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package estatuas;
-import java.util.Scanner;
-import java.util.Arrays;
-/**
- *
- * @author Priscila
- */
-public class Estatuas {
+public class Esfera{
+	private double radio;
+	private double pi = 3.1416;
 
-    public static void main(String[] args) {
-        //Scanner y entrada de datos, creacion del arreglo y datos de resultados
-    Scanner sc = new Scanner(System.in);
-    System.out.println("De el tamaño del arreglo:");
-    int tam = sc.nextInt();
-    int[] tamaños = new int[tam];
-    System.out.println("De los tamaños de las estatuas");
-    for (int i=0 ; i<tam ; i++){ //Lectura de datos de arreglo
-        int x;
-        x= sc.nextInt();
-        tamaños[i] = x;
-       }
-    Arrays.sort(tamaños); //Se ordena el arreglo para que sea más facil el proceso
-    Estatuas.faltantes(tamaños,tam); //Manda llamar el metodo que arroja estatuas faltantes
-    }
-    
-   public static void faltantes(int[] tamaños, int tam){ //Firma del metodo
-       int total = 0;
-       for (int i = 0; i < tam-1; i++){
-           if(tamaños[i] != tamaños[i+1]-1) {
-           do{
-               total++;
-           }while(tamaños[i]+total != tamaños[i+1]-1);
-           }
-       }
-       System.out.println("Faltan "+total+ " estatuas");
-}
+	public Esfera(double radio){
+		this.radio = radio;
+	}
+
+	public void setEsfera(double radio){
+		this.radio = radio;
+	}
+
+	public double area(){
+        return 4*pi*(Math.pow(radio,2));
+	}
+
+	public double volumen(){
+		return (4/3)*pi*(Math.pow(radio,3));
+	}
+
+	public void imprimirArea(){
+		System.out.println("El area de la esfera es: " + area());
+	}
+	 public void imprimirVolumen(){
+	 	System.out.println("El volumen de la esfera es: " + volumen());
+	 }
 }
